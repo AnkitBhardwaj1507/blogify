15039f40f697f35919caf936fab618a01b8f95f2
AnkitBhardwaj1507 <bhardwaj.ankit31@gmail.com>
2023-11-12 00:57:37 +0530

    push bolgify code

---
+++ services/authentication.js
@@ -0,0 +1,27 @@
+const JWT = require("jsonwebtoken");
+
+const secret = process.env.TOKEN || "asdfghjkl";
+
+function createTokenForUser(user) {
+    const payload = {
+        _id: user._id,
+        email: user.email,
+        profileImageURL: user.profileImageURL,
+        role: user.role,
+        name: user.fullName,
+    };
+
+    const token = JWT.sign(payload, secret);
+    return token;
+
+}
+
+function validateToken(token) {
+    const payload = JWT.verify(token, secret);
+    return payload;
+}
+
+module.exports = {
+    createTokenForUser,
+    validateToken
+}
